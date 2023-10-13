fc4892957534b9e5b6b867ec3c23bafd35023aaa
MukeshPullabhatla <amukeshps@gmail.com>
2023-10-13 00:14:08 -0400

    Added Two Sum brute force

--- TwoSum1.py
+++ TwoSum1.py
@@ -6,6 +6,13 @@ def twoSum(nums, target):
         else:
             val[target-num]=i
 
+def twoSumBruteForce(nums, target):
+    for i in range(len(nums)-1):
+        for j in range(i+1, len(nums)):
+            if target == nums[i] + nums[j]:
+                return i, j
+
 nums = [2,7,11,15]
 target = 9
 print(twoSum(nums, target))
+print(twoSumBruteForce(nums, target))
