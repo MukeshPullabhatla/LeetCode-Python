c0c53ad789bddff4fb4074f2ece1f8f890f00d8b
MukeshPullabhatla <amukeshps@gmail.com>
2023-10-18 00:35:12 -0400

    Diagonal Traversal

---
+++ DiagonalTraverse498.py
@@ -0,0 +1,25 @@
+def findDiagonalOrder(mat):
+    if not mat or not mat[0]:
+        return []
+    n, m = len(mat), len(mat[0])
+    result, intermediate = [], []
+    for i in range(n+m-1):
+        intermediate.clear()
+        r = 0 if i<m else i-m+1
+        c = i if i<m else m-1
+        while r<n and c>-1:
+            intermediate.append(mat[r][c])
+            r+=1
+            c-=1
+        if i%2==0:
+            result.extend(intermediate[::-1])
+        else:
+            result.extend(intermediate)
+    return result
+
+mat = [[1,2,3],[4,5,6],[7,8,9]]
+mat1 = [[1,2],[3,4]]
+mat2 = [[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]]
+print(findDiagonalOrder(mat))
+print(findDiagonalOrder(mat1))
+print(findDiagonalOrder(mat2))
