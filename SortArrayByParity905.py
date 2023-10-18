e8ef792d80f5d7b150a5c1e49fb9b7186134db44
MukeshPullabhatla <amukeshps@gmail.com>
2023-10-18 16:40:13 -0400

    Sort Array By Parity - Second and Optimal Approach

--- SortArrayByParity905.py
+++ SortArrayByParity905.py
@@ -9,8 +9,33 @@ def sortArrayByParity(nums):
         if num%2!=0:
             res.append(num)
     return res
+# Second Approach
+def sortArrayByParitySecond(nums):
+    even, odd = [], []
+    for i in range(len(nums)):
+        if nums[i]%2==0:
+            even.append(nums[i])
+        else:
+            odd.append(nums[i])
+    return even+odd
+
+# Optimal Approach
+def sortArrayByParityOptimal(nums):
+    left, right = 0, len(nums)-1
+    while left<right:
+        if nums[left]%2==1 and nums[right]%2==0:
+            nums[left], nums[right] = nums[right], nums[left]
+        if nums[left]%2==0:
+            left+=1
+        if nums[right]%2==1:
+            right-=1
+    return nums
 
 nums = [3,1,2,4]
 nums1 = [0]
 print(sortArrayByParity(nums))
 print(sortArrayByParity(nums1))
+print(sortArrayByParitySecond(nums))
+print(sortArrayByParitySecond(nums1))
+print(sortArrayByParityOptimal(nums))
+print(sortArrayByParityOptimal(nums1))
