66db8a46c744b4db4467581e3533e69f57b3ab62
MukeshPullabhatla <amukeshps@gmail.com>
2023-10-18 10:04:52 -0400

    Diagonal traversal variables change

--- DiagonalTraverse498.py
+++ DiagonalTraverse498.py
@@ -1,20 +1,17 @@
 def findDiagonalOrder(mat):
-    if not mat or not mat[0]:
-        return []
+    if not mat or not mat[0]: return []
     n, m = len(mat), len(mat[0])
-    result, intermediate = [], []
+    result, res = [], []
     for i in range(n+m-1):
-        intermediate.clear()
+        res.clear()
         r = 0 if i<m else i-m+1
         c = i if i<m else m-1
         while r<n and c>-1:
-            intermediate.append(mat[r][c])
+            res.append(mat[r][c])
             r+=1
             c-=1
-        if i%2==0:
-            result.extend(intermediate[::-1])
-        else:
-            result.extend(intermediate)
+        if i%2==0: result.extend(res[::-1])
+        else: result.extend(res)
     return result
 
 mat = [[1,2,3],[4,5,6],[7,8,9]]
