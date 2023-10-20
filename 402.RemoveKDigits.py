b8a369186b41d2bb39f28bbbf7f72856c6c5613f
MukeshPullabhatla <amukeshps@gmail.com>
2023-10-20 15:06:05 -0400

    Remove K Digits

--- 402.RemoveKDigits.py
+++ 402.RemoveKDigits.py
@@ -1,4 +1,4 @@
-def removeKDigits(num: str, k: int):
+def removeKdigits(num: str, k: int) -> str:
     stack = list()
     for n in num:
         while stack and k and stack[-1]>n:
@@ -9,15 +9,12 @@ def removeKDigits(num: str, k: int):
     if k:
         stack = stack[0:-k]
     return ''.join(stack) or '0'
-
-
 num = "1432219"
 k = 3
 num1 = "10200"
 k1 = 1
 num2 = "10"
 k2 = 2
-
-print(removeKDigits(num, k))
-print(removeKDigits(num1, k1))
-print(removeKDigits(num2, k2))
+print(removeKdigits(num, k))
+print(removeKdigits(num1, k1))
+print(removeKdigits(num2, k2))
