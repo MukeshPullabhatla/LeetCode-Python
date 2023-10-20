c79b5ad59cc8fdd8275831d01b239d582a6fe831
MukeshPullabhatla <amukeshps@gmail.com>
2023-10-20 00:51:22 -0400

    changing variable name

--- StringToInteger8.py
+++ StringToInteger8.py
@@ -1,34 +1,35 @@
 def myAtoi(s:str) -> int:
-    value, state, pos, sign = 0, 0, 0, 1
-    if len(s) == 0:
-        return 0
-    while pos<len(s):
-        curr_char = s[pos]
+    value, state, position, sign = 0, 0, 0, 1
+    if len(s) == 0: return 0
+    while position<len(s):
+        current_character = s[position]
         if state == 0:
-            if curr_char == " ":
+            if current_character == " ":
                 state = 0
-            elif curr_char == "+" or curr_char == "-":
+            elif current_character == "+" or current_character == "-":
                 state = 1
-                sign = 1 if curr_char == "+" else -1
-            elif curr_char.isdigit():
+                sign = 1 if current_character == "+" else -1
+            elif current_character.isdigit():
                 state = 2
-                value = value * 10 + int(curr_char)
+                value = value * 10 + int(current_character)
             else:
                 return 0
         elif state == 1:
-            if curr_char.isdigit():
+            if current_character.isdigit():
                 state = 2
-                value = value * 10 + int(curr_char)
-            else: return 0
+                value = value * 10 + int(current_character)
+            else:
+                return 0
         elif state == 2:
-            if curr_char.isdigit():
+            if current_character.isdigit():
                 state = 2
-                value = value * 10 + int(curr_char)
+                value = value * 10 + int(current_character)
             else:
                 break
         else:
             return 0
-        pos += 1
+        position += 1
+
     value = sign*value
     value = min(value, 2**31-1)
     value = max(-(2**31), value)
