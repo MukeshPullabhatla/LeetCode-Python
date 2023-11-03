fca498f8a198c3b4e9a354dd1a5f1fc0e1efc273
MukeshPullabhatla <amukeshps@gmail.com>
2023-11-03 18:21:35 -0400

    approach 2

--- 1657.DetermineIfTwoStringsAreClose.py
+++ 1657.DetermineIfTwoStringsAreClose.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def closeStrings(word1: str, word2: str) -> bool:
     if len(word1) != len(word2): return False
     word1Map = {}
@@ -10,6 +13,12 @@ def closeStrings(word1: str, word2: str) -> bool:
     word1Frequency = sorted(word1Map.values())
     word2Frequency = sorted(word2Map.values())
     return word1Frequency == word2Frequency
+def closeStrings1(word1: str, word2: str) -> bool:
+    w1 = Counter(word1)
+    w2 = Counter(word2)
+    n1 = Counter([v for v in w1.values()])
+    n2 = Counter([v for v in w2.values()])
+    return w1 == w2 or (n1 == n2 and set(word1) == set(word2))
 
 word1 = "abc"
 word2 = "bca"
@@ -20,3 +29,6 @@ word6 = "abbccc"
 print(closeStrings(word1, word2))
 print(closeStrings(word3, word4))
 print(closeStrings(word5, word6))
+print(closeStrings1(word1, word2))
+print(closeStrings1(word3, word4))
+print(closeStrings1(word5, word6))
