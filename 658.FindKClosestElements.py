003e8d4f433ecb68be2a386d75f9e05b05e2f041
MukeshPullabhatla <amukeshps@gmail.com>
2023-11-08 11:22:26 -0500

    using binary search

---
+++ 658.FindKClosestElements.py
@@ -0,0 +1,26 @@
+from typing import List
+# Using Custom Comparator
+def findClosestElements(arr: List[int], k: int, x: int) -> List[int]:
+    sorted_arr = sorted(arr, key = lambda num: abs(x-num))
+    result = []
+    for i in range(k):
+        result.append(sorted_arr[i])
+    return sorted(result)
+
+arr = [1,2,3,4,5]
+k = 4
+x = 3
+print(findClosestElements(arr, k, x))
+
+# Using binary search
+def findClosestElements1(arr: List[int], k: int, x: int) -> List[int]:
+    left = 0
+    right = len(arr) - k
+    while left < right:
+        mid = (left + right) //2
+        if x - arr[mid] > arr[mid + k] - x:
+            left = mid + 1
+        else:
+            right = mid
+    return arr[left:left+k]
+print(findClosestElements1(arr, k, x))
